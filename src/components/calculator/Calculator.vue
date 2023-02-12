3b03cc9f0bf9b563b41964762a36098a80b61048
Héctor Martínez <hector.martinez@the-cocktail.com>
2023-02-12 19:31:56 +0100

    calculator improvements

--- src/components/calculator/Calculator.vue
+++ src/components/calculator/Calculator.vue
@@ -1,51 +1,57 @@
 <template>
   <div class="calculator">
-    <div class="calculator__output">{{ screenValue }}</div>
-    <div class="calculator__button" @click="clickButton('1')">1</div>
-    <div class="calculator__button" @click="clickButton('2')">2</div>
-    <div class="calculator__button" @click="clickButton('3')">3</div>
-    <div class="calculator__button orange" @click="clickButton('/')">÷</div>
-    <div class="calculator__button" @click="clickButton('4')">4</div>
-    <div class="calculator__button" @click="clickButton('5')">5</div>
-    <div class="calculator__button" @click="clickButton('6')">6</div>
-    <div class="calculator__button orange" @click="clickButton('*')">x</div>
-    <div class="calculator__button" @click="clickButton('7')">7</div>
-    <div class="calculator__button" @click="clickButton('8')">8</div>
-    <div class="calculator__button" @click="clickButton('9')">9</div>
-    <div class="calculator__button orange" @click="clickButton('-')">-</div>
-    <div class="calculator__button" @click="clickButton('0')">0</div>
-    <div class="calculator__button orange" @click="clickButton('C')">C</div>
-    <div class="calculator__button orange" @click="clickButton('=')">=</div>
-    <div class="calculator__button orange" @click="clickButton('+')">+</div>
+    <p
+      class="calculator__output"
+      :class="{ small: result.length > 9 || result.toString().length > 9 }"
+    >
+      {{ screenValue }}
+    </p>
+    <div @click="clickButton('1')">1</div>
+    <div @click="clickButton('2')">2</div>
+    <div @click="clickButton('3')">3</div>
+    <div class="orange" @click="clickButton('/')">÷</div>
+    <div @click="clickButton('4')">4</div>
+    <div @click="clickButton('5')">5</div>
+    <div @click="clickButton('6')">6</div>
+    <div class="orange" @click="clickButton('*')">x</div>
+    <div @click="clickButton('7')">7</div>
+    <div @click="clickButton('8')">8</div>
+    <div @click="clickButton('9')">9</div>
+    <div class="orange" @click="clickButton('-')">-</div>
+    <div @click="clickButton('0')">0</div>
+    <div class="orange" @click="clickButton('C')">C</div>
+    <div class="orange" @click="clickButton('=')">=</div>
+    <div class="orange" @click="clickButton('+')">+</div>
   </div>
 </template>
 
 <script lang="ts" setup>
   /* imports */
-
   import { ref, computed } from 'vue';
+
   /* access var */
-  const log = ref('');
+  const result = ref('');
 
   /* Computed */
+  // inferred type: ComputedRef<number>
   const screenValue = computed(() => {
-    if (log.value == '') return 0;
-    else return log.value;
+    if (result.value == '') return 0;
+    else return result.value;
   });
 
   /* Methods */
-  const clickButton = (e: any) => {
+  const clickButton = (e: string) => {
     if (e == 'C') {
       clear();
     } else if (e == '=') {
-      log.value = eval(log.value);
+      result.value = eval(result.value);
     } else {
-      log.value = log.value + e;
+      result.value = result.value + e;
     }
   };
 
   const clear = () => {
-    log.value = '';
+    result.value = '';
   };
 </script>
 
